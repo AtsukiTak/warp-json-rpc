7eba807e8b29584f3d0a0e94343aaaf4b15c50c6
AtsukiTak <takatomgoo@gmail.com>
2020-02-14 11:53:37 +0900

    add deserialize request test

--- src/req.rs
+++ src/req.rs
@@ -21,7 +21,62 @@ pub enum Version {
 }
 
 #[derive(Debug, Deserialize)]
+#[serde(untagged)]
 pub enum Params {
     ByPosition(Vec<Value>),
     ByName(Map<String, Value>),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn deserialize_by_name_request() {
+        let req_str = r#"{
+            "jsonrpc": "2.0",
+            "method": "op",
+            "params": { "lhs": 24, "rhs": 12, "op": "+"},
+            "id": 42
+        }"#;
+        let req = serde_json::from_str::<Request>(req_str).unwrap();
+        assert_eq!(req.id, Some(42));
+        assert_eq!(req.method, "op".to_string());
+
+        match req.params.unwrap() {
+            Params::ByName(map) => {
+                assert_eq!(map.get("lhs"), Some(&json!(24)));
+                assert_eq!(map.get("rhs"), Some(&json!(12)));
+                assert_eq!(map.get("op"), Some(&json!("+")));
+            }
+            Params::ByPosition(_) => {
+                panic!("Error: parameter should be ByName");
+            }
+        }
+    }
+
+    #[test]
+    fn deserialize_by_pos_request() {
+        let req_str = r#"{
+            "jsonrpc": "2.0",
+            "method": "op",
+            "params": [24, 12, "+"],
+            "id": 42
+        }"#;
+        let req = serde_json::from_str::<Request>(req_str).unwrap();
+        assert_eq!(req.id, Some(42));
+        assert_eq!(req.method, "op".to_string());
+
+        match req.params.unwrap() {
+            Params::ByPosition(arr) => {
+                assert_eq!(arr[0], json!(24));
+                assert_eq!(arr[1], json!(12));
+                assert_eq!(arr[2], json!("+"));
+            }
+            Params::ByName(_) => {
+                panic!("Error: parameter should be ByPosition");
+            }
+        }
+    }
 }
