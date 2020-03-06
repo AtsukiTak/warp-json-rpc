fff4cd537dd9fcb4d7bc31eea33c0061808cb7a5
AtsukiTak <takatomgoo@gmail.com>
2020-03-06 11:49:09 +0900

    add some filters

--- src/filter.rs
+++ src/filter.rs
@@ -1,21 +1,20 @@
 use crate::{res::HyperResponse, Request, Server};
+use futures::future;
 use warp::{filters, Filter, Rejection};
 
-pub fn json_rpc(
-    server: Server,
-) -> impl Filter<Extract = (HyperResponse,), Error = Rejection> + Clone {
+/// Create a `Filter` that initialize JSON RPC handling.
+pub fn json_rpc(server: Server) -> impl Filter<Extract = (), Error = Rejection> + Clone {
     filters::method::post()
-        .and(warp::filters::header::exact(
-            "Content-Type",
-            "application/json",
-        ))
-        .and(filters::body::json())
-        .and_then(move |req: Request| {
-            let server2 = server.clone();
-            async move {
-                let res = server2.handle_request(req).await;
-                let hyper_res = (&res).into();
-                Result::<_, Rejection>::Ok(hyper_res)
-            }
+        .and(filters::header::exact("Content-Type", "application/json"))
+        // Get and set `Request` if it is not stored already.
+        .and(store::filled().or(store_req()))
+}
+
+fn store_req() -> impl Filter<Extract = (Request,), Error = Rejection> {
+    filters::body::json::<Request>()
+        .and(lazy_store())
+        .map(|req, store| {
+            store.fill(req.clone());
+            req
         })
 }
--- src/store.rs
+++ src/store.rs
@@ -26,3 +26,35 @@ impl LazyReqStore {
         self.store.borrow()
     }
 }
+
+pub mod filters {
+    /// Create a `Filter` that extracts `LazyReqStore`.
+    pub fn store() -> impl Filter<Extract = (LazyReqStore,), Error = Rejection> {
+        filters::ext::optional::<LazyReqStore>().and_then(|opt| {
+            match opt {
+                Some(store) => future::ok(store),
+                None => {
+                    log::error!("Your Filter has to be wrapped by `JsonRpcService`");
+                    // TODO return custom error
+                    future::err(reject::reject())
+                }
+            };
+        })
+    }
+
+    /// Create a `Filter` that requires the `LazyReqStore` is already filled.
+    pub fn filled() -> impl Filter<Extract = (), Error = Rejection> {
+        store().and_then(|store| {
+            if store.filled() {
+                future::ok(())
+            } else {
+                future::err(reject::reject())
+            }
+        })
+    }
+
+    /// Create a `Filter` that extracts stored `Request`.
+    pub fn stored_req() -> impl Filter<Extract = (Request,), Error = Rejection> {
+        get_store().and_then(|store| future::ready(store.borrow().cloned().ok_or(reject::reject())))
+    }
+}
