78489e3a0b801665beaf10f304d42b8cb821f01f
AtsukiTak <takatomgoo@gmail.com>
2020-03-08 16:22:50 +0900

    add top level module document

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,32 @@
+//! # Usage
+//!
+//! ```rust
+//! use warp_json_rpc::filters as json_rpc;
+//! use futures::future;
+//! use warp::Filter as _;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!   // create Filter
+//!   let route = warp::filters::path::path("rpc")
+//!     // ## Point 1
+//!     // This filter is required.
+//!     .and(json_rpc::json_rpc())
+//!     .and(json_rpc::method("add"))
+//!     .and(json_rpc::params::<(usize, usize)>())
+//!     // `res.success` returns `impl Reply` which represents JSON RPC Response
+//!     .map(|res: Builder, (lhs, rhs)| res.success(lhs + rhs).unwrap());
+//!
+//!   // ## Point 2
+//!   // You **MUST** wraps root `Filter` by `warp_json_rpc::service` function.
+//!   let svc = warp_json_rpc::service(route);
+//!   let make_svc = hyper::service::make_service_fn(move |_| future::ok::<_, Infallible>(svc));   
+//!   hyper::Server::bind(&([127, 0, 0, 1], 3030).into())
+//!     .serve(make_svc)
+//!     .await
+//!     .unwrap();
+//! }
+//! ```
 pub mod filters;
 mod req;
 mod res;
