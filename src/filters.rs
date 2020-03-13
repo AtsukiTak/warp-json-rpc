b06aafbce98a3eff80808d2652e5ba071a110d85
AtsukiTak <takatomgoo@gmail.com>
2020-03-13 17:58:41 +0900

    fix log format

--- src/filters.rs
+++ src/filters.rs
@@ -57,7 +57,7 @@ pub fn method(name: &'static str) -> impl Filter<Extract = (), Error = Rejection
     store::stored_req()
         .and_then(move |req: Request| {
             if req.method() == name {
-                log::info!(target: "warp_json_rpc", "match method \"{}\"", name);
+                log::info!(target: "warp_json_rpc", "\"{}\" RPC", name);
                 future::ok(())
             } else {
                 future::err(reject::reject())
